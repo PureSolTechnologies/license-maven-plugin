e3508b6482a6f225e973d631a6ed918b3ed1ab77
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-11-30 23:18:22 +0100

    Renamed packages to reflect groupId.

--- src/main/java/com/puresoltechnologies/maven/plugins/license/ValidationResult.java
+++ src/main/java/com/puresoltechnologies/maven/plugins/license/ValidationResult.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.puresol.maven.plugins.license;
+package com.puresoltechnologies.maven.plugins.license;
 
 /**
  * This enum specifies the result of the license validation.
--- src/main/java/com/puresoltechnologies/maven/plugins/license/ValidatorMojo.java
+++ src/main/java/com/puresoltechnologies/maven/plugins/license/ValidatorMojo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.puresol.maven.plugins.license;
+package com.puresoltechnologies.maven.plugins.license;
 
 import java.util.List;
 import java.util.Set;
